e2cd88fefd65f953e284723b9a4000ae69703c0e
KFC.Hook <rvzdxkngjz@privaterelay.appleid.com>
2026-05-26 00:05:40 +1000

    Update Scrlpts.lua

--- Scrlpts.lua
+++ Scrlpts.lua
@@ -1,5 +1,5 @@
 
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KFChook/BoronZ/refs/heads/main/keysystem.lua"))()
 loadstring(game:HttpGet(""))()
 loadstring(game:HttpGet(""))()
 loadstring(game:HttpGet(""))()
